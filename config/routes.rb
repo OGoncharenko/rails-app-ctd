04793b1879865c4447928179a244fb10e5defedb
OGoncharenko <olyas.goncharenko@gmail.com>
2025-10-14 14:04:40 -0700

    Fix lint

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :categories, only: %i[index show]
   resources :users, only: %i[show]
   get "mylists", to: "lists#mylists", as: :"lists/mylists"
-      # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
